f8c52927c272970bcc80e90b19ed3062c6fb4a94
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-02-05 16:01:42 -0800

    Add paddle inputs

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
 });
 
 var ball;
+var paddle;
 
 function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -10,6 +11,7 @@ function preload() {
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = '#eee';
   game.load.image('ball', 'img/ball.png');
+  game.load.image('paddle', 'img/paddle.png');
 }
 
 function create() {
@@ -19,7 +21,13 @@ function create() {
   ball.body.collideWorldBounds = true;
   ball.body.bounce.set(1);
   ball.body.velocity.set(150, 150);
+  paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
+  paddle.anchor.set(0.5, 1);
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  paddle.body.immovable = true;
 }
 
 function update() {
+  game.physics.arcade.collide(ball, paddle);
+  paddle.x = game.input.x || game.world.width * 0.5;
 }
